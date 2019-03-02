d6fcd2d8fa78ee9dfb9534e68d271816ba6c40d8
Farzam Noori <fnoori431@gmail.com>
2019-03-02 11:20:17 -0700

    Added routes that use query builder

--- api/controllers/cars.js
+++ api/controllers/cars.js
@@ -10,6 +10,32 @@ exports.get_all_cars = (req, res, next) => {
   });
 }
 
+exports.get_all_cars_by_make_and_year = (req, res, next) => {
+  let make = req.params.make;
+  let year = req.params.year;
+
+  CarsModel.find({ 'Make': make })
+  .where('Year').equals(year)
+  .exec().then(cars => {
+    res.status(201).json(cars);
+  }).catch(findErr => {
+    return res.status(500).json({ 'Error': findErr });
+  });
+}
+
+exports.get_all_cars_by_make_and_greater_than_doors_less_than_doors = (req, res, next) => {
+  let moreThanDoors = req.params.moreThanDoors;
+  let lessThanDoors = req.params.lessThanDoors;
+
+  CarsModel.find()
+  .where('Doors').gt(moreThanDoors).lt(lessThanDoors)
+  .exec().then(cars => {
+    res.status(201).json(cars);
+  }).catch(findErr => {
+    return res.status(500).json({ 'Error': findErr });
+  });
+}
+
 exports.add_new_car = (req, res, next) => {
   console.log(`make length: ${req.body.make.length}`);
 
--- api/routes/cars.js
+++ api/routes/cars.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const carsController = require('../controllers/cars');
 
 router.get('/get_all_cars', carsController.get_all_cars);
+router.get('/get_all_cars_by_make_and_year/:make/:year', carsController.get_all_cars_by_make_and_year);
+router.get('/get_all_cars_by_make_and_greater_than_doors_less_than_doors/:moreThanDoors/:lessThanDoors', carsController.get_all_cars_by_make_and_greater_than_doors_less_than_doors);
 
 router.post('/add_new_car', carsController.add_new_car);
 router.post('/add_new_car_user', )
