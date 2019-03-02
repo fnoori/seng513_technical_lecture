2079ec19ab2b73ea6428f6fd8d285734595121cf
Farzam Noori <fnoori431@gmail.com>
2019-03-02 12:49:32 -0700

    minor spaces to code

--- api/models/cars.js
+++ api/models/cars.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const vehicleSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    'Make': {type: String, required: true, maxlength: 9},
-    'Model': {type: String, required: true, maxlength: 9},
-    'Trim': {type: String, required: true, maxlength: 10},
-    'Price': {type: String, required: true, maxlength: 20},
-    'Year': {type: String, required: true, maxlength: 4},
-    'Doors': {type: Number, required: true}
+    'Make': { type: String, required: true, maxlength: 9 },
+    'Model': { type: String, required: true, maxlength: 9 },
+    'Trim': { type: String, required: true, maxlength: 10 },
+    'Price': { type: String, required: true, maxlength: 20 },
+    'Year': { type: String, required: true, maxlength: 4},
+    'Doors': { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Car', vehicleSchema);
