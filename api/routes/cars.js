16fb728583f14fe186de731a0d643072213c3831
Farzam Noori <fnoori431@gmail.com>
2019-03-19 10:37:53 -0600

    clearified more code

--- api/routes/cars.js
+++ api/routes/cars.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+
+// import controller
 const carsController = require('../controllers/cars');
 
+
+// define routes
 router.get('/get_all_cars', carsController.get_all_cars);
+
 router.get('/get_all_cars_by_make_and_year/:make/:year', carsController.get_all_cars_by_make_and_year);
+
 router.get('/get_all_cars_by_make_and_greater_than_doors_less_than_doors/:moreThanDoors/:lessThanDoors', carsController.get_all_cars_by_make_and_greater_than_doors_less_than_doors);
 
+
 router.post('/add_new_car', carsController.add_new_car);
+
 router.post('/add_new_car_user', )
+
 
 module.exports = router;
