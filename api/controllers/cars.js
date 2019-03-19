30c5f0a758751b99d60ee57f0bbcc4953cdad2d8
Farzam Noori <fnoori431@gmail.com>
2019-03-19 10:36:31 -0600

    added comments and removed comments
    made code more readable by adding spaces

--- api/controllers/cars.js
+++ api/controllers/cars.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 const CarsModel = require('../models/cars');
 
 exports.get_all_cars = (req, res, next) => {
+
   CarsModel.find().exec()
   .then(cars => {
+
     res.status(201).json(cars);
+
   }).catch(findErr => {
+
     return res.status(500).json({'error': findErr});
+
   });
+
 }
 
 exports.get_all_cars_by_make_and_year = (req, res, next) => {
@@ -15,11 +21,17 @@ exports.get_all_cars_by_make_and_year = (req, res, next) => {
   let year = req.params.year;
 
   CarsModel.find({ 'Make': make })
+
   .where('Year').equals(year)
+
   .exec().then(cars => {
+
     res.status(201).json(cars);
+
   }).catch(findErr => {
+
     return res.status(500).json({ 'Error': findErr });
+
   });
 }
 
@@ -28,16 +40,21 @@ exports.get_all_cars_by_make_and_greater_than_doors_less_than_doors = (req, res,
   let lessThanDoors = req.params.lessThanDoors;
 
   CarsModel.find()
+
   .where('Doors').gt(moreThanDoors).lt(lessThanDoors)
+
   .exec().then(cars => {
+
     res.status(201).json(cars);
+
   }).catch(findErr => {
+
     return res.status(500).json({ 'Error': findErr });
+
   });
 }
 
 exports.add_new_car = (req, res, next) => {
-  console.log(`make length: ${req.body.make.length}`);
 
   let newCar = new CarsModel({
     '_id': new mongoose.Types.ObjectId(),
@@ -50,9 +67,14 @@ exports.add_new_car = (req, res, next) => {
   });
 
   newCar.save()
+
   .then(carSaved => {
+
     res.status(201).json(carSaved);
+
   }).catch(saveErr => {
+
     return res.status(500).json({'Error': saveErr});
+
   });
 }
